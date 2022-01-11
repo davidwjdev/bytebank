e3802bc7a914894ab686324a7671b3a5a9e54fc7
David W de Jesus <davidwj15@gmail.com>
2022-01-11 14:25:40 -0300

    adicionado service

--- src/app/extrato/extrato.component.ts
+++ src/app/extrato/extrato.component.ts
@@ -1,3 +1,4 @@
+import { TransferenciaService } from './../services/transferencia.service';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
@@ -10,9 +11,10 @@ export class ExtratoComponent implements OnInit {
   @Input() transferencias: any[];
 
 
-  constructor() { }
+  constructor(private service: TransferenciaService) { }
 
   ngOnInit(): void {
+    this.transferencias = this.service.transferencias;
   }
 
 }
